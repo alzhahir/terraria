2ea1e68ee0132cd13574620fbae3254fa6d07932
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2025-01-08 11:20:10 +0800

    Update entrypoint.sh

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -4,6 +4,10 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+#export DOTNET_ROOT=/usr/lib/dotnet/
+printf "dotnet version: "
+dotnet --version
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
